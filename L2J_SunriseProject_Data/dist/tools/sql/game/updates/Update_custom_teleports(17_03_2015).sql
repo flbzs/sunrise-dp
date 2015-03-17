d42458beafb6dfae69da816c3cd6f1d7ee132958
vGodFather <vGodFather@7628a37d-9d16-49c9-a037-77158a4058ea>
2015-03-17 06:20:16 +0000

    For live servers Execute only: Update_custom_teleports(17_03_2015).sql
    Located: tools/sql/game/updates/

--- L2J_SunriseProject_Data/dist/tools/sql/game/updates/Update_custom_teleports(17_03_2015).sql
+++ L2J_SunriseProject_Data/dist/tools/sql/game/updates/Update_custom_teleports(17_03_2015).sql
@@ -1,1 +1,3 @@
 ALTER TABLE `custom_teleports` ADD `onlyForNoble` TINYINT UNSIGNED DEFAULT 0;
+ALTER TABLE `custom_teleports` ADD `itemIdToGet` int(10) UNSIGNED DEFAULT 57;
+ALTER TABLE `custom_teleports` ADD `teleportPrice` int(10) UNSIGNED DEFAULT 1000;
