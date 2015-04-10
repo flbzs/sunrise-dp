b1b0d363acd2a8db20e868c252bd7a685542b7be
vGodFather <vGodFather@7628a37d-9d16-49c9-a037-77158a4058ea>
2015-04-10 03:17:19 +0000

    Fixed some geodata issues allow hitting through floors Fixed Sweep issue with premium spoil rates
    Core Changes:
    compile and copy paste core.jar
    
    Data changes:
    delete scripts folder and paste from this rev

--- L2J_SunriseProject_Data/dist/game/data/scripts/handlers/effecthandlers/Sweeper.java
+++ L2J_SunriseProject_Data/dist/game/data/scripts/handlers/effecthandlers/Sweeper.java
@@ -70,7 +70,15 @@ public class Sweeper extends L2Effect
 			}
 			else
 			{
-				player.addItem("Sweeper", item, getEffected(), true);
+				int itemId = item.getId();
+				long itemCount = item.getCount();
+				
+				if (player.isPremium())
+				{
+					itemCount *= player.calcPremiumDropMultipliers(itemId);
+				}
+				
+				player.addItem("Sweeper", itemId, itemCount, getEffected(), true);
 			}
 		}
 		return true;
