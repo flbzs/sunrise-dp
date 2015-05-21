7226ebf16900bb8657418086b19736422764e581
vgodfather <vgodfather@7628a37d-9d16-49c9-a037-77158a4058ea>
2015-05-21 21:16:07 +0000

    Minor typo for some admin commands
    compile and copy paste core.jar

--- L2J_SunriseProject_Data/dist/game/data/scripts/handlers/admincommandhandlers/AdminCHSiege.java
+++ L2J_SunriseProject_Data/dist/game/data/scripts/handlers/admincommandhandlers/AdminCHSiege.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2013 L2J DataPack
+ * Copyright (C) 2004-2015 L2J DataPack
  * 
  * This file is part of L2J DataPack.
  * 
@@ -292,8 +292,7 @@ public final class AdminCHSiege implements IAdminCommandHandler
 			}
 			else
 			{
-				ClanHallSiegeEngine siegable = hall.getSiege();
-				siegable.getAttackers().clear();
+				hall.getSiege().getAttackers().clear();
 			}
 		}
 		else if (split[0].equals(COMMANDS[7]))
@@ -357,7 +356,7 @@ public final class AdminCHSiege implements IAdminCommandHandler
 	
 	private void sendSiegableHallPage(L2PcInstance activeChar, String hallId, SiegableHall hall)
 	{
-		NpcHtmlMessage msg = new NpcHtmlMessage();
+		final NpcHtmlMessage msg = new NpcHtmlMessage();
 		msg.setFile(null, "data/html/admin/siegablehall.htm");
 		msg.replace("%clanhallId%", hallId);
 		msg.replace("%clanhallName%", hall.getName());
