20be145b3e0355ce1ce1549daa793ef7ca517739
vGodFather <vGodFather@7628a37d-9d16-49c9-a037-77158a4058ea>
2016-05-20 08:48:13 +0000

    Fixed some null pointer Fixed missing htmls
    Delete scripts folder and paste from this rev

--- L2J_SunriseProject_Data/dist/game/data/scripts/conquerablehalls/FortressOfResistance/FortressOfResistance.java
+++ L2J_SunriseProject_Data/dist/game/data/scripts/conquerablehalls/FortressOfResistance/FortressOfResistance.java
@@ -47,7 +47,7 @@ public final class FortressOfResistance extends ClanHallSiegeEngine
 		new Location(45109, 112124, -1900), // 30%
 		new Location(47653, 110816, -2110), // 40%
 		new Location(47247, 109396, -2000)
-	// 30%
+		// 30%
 	};
 	
 	private L2Spawn _nurka;
