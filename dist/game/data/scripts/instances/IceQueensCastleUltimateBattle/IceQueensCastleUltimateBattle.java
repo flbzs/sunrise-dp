8ecb7317f22b4d4ec6621311df15d6aea0b6f545
vGodFather <vGodFather@7628a37d-9d16-49c9-a037-77158a4058ea>
2016-11-01 07:34:26 +0000

    Fixed some issues with freya instance target players behind walls
    Delete scripts folder and paste from this rev

--- dist/game/data/scripts/instances/IceQueensCastleUltimateBattle/IceQueensCastleUltimateBattle.java
+++ dist/game/data/scripts/instances/IceQueensCastleUltimateBattle/IceQueensCastleUltimateBattle.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import l2r.Config;
+import l2r.gameserver.GeoData;
 import l2r.gameserver.enums.CtrlIntention;
 import l2r.gameserver.enums.MountType;
 import l2r.gameserver.enums.PcCondOverride;
@@ -1252,7 +1253,7 @@ public final class IceQueensCastleUltimateBattle extends AbstractInstance
 		final List<L2PcInstance> players = new ArrayList<>();
 		for (L2PcInstance player : world.playersInside)
 		{
-			if ((player != null) && !player.isDead() && (player.getInstanceId() == world.getInstanceId()) && !player.isInvisible())
+			if ((player != null) && !player.isDead() && GeoData.getInstance().canMove(mob, player) && (player.getInstanceId() == world.getInstanceId()) && !player.isInvisible())
 			{
 				players.add(player);
 			}
